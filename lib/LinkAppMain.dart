28357e5899bcfebe41553969c2d1f745d69dbe3e
MMattLeeE <mmattleee@gmail.com>
2021-08-01 10:37:12 -0400

    auth and get transactions implemented

--- lib/LinkAppMain.dart
+++ lib/LinkAppMain.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:plaid_flutter/plaid_flutter.dart';
 
 import 'package:cloud_functions/cloud_functions.dart';
+import 'dart:convert';
 
 class LinkAppMain extends StatefulWidget {
   const LinkAppMain({Key? key}) : super(key: key);
@@ -13,7 +14,17 @@ class LinkAppMain extends StatefulWidget {
 class _LinkAppMainState extends State<LinkAppMain> {
   late PlaidLink _plaidLinkToken;
   String _linkToken = 'Getting Link Token...';
+  String _accessToken = '';
+  String _itemId = '';
+  List<dynamic> _transactionData = [
+    {
+      'date': '1/2/3',
+      'merchant_name': 'default',
+      'amount': 200,
+    }
+  ];
   bool _linkReady = false;
+  bool _auth = false;
 
   // explaination for how futures/promises work :
   // https://dart.dev/codelabs/async-await
@@ -51,7 +62,22 @@ class _LinkAppMainState extends State<LinkAppMain> {
     print(_linkToken);
   }
 
+  void _exchangePublicToken(String publicToken) async {
+    HttpsCallable callable =
+        FirebaseFunctions.instance.httpsCallable('exchangePublicToken');
+    final message = await callable({'publicToken': publicToken});
+    setState(() {
+      _accessToken = message.data['access_token'];
+      _itemId = message.data['item_id'];
+      _auth = true;
+    });
+    print(_accessToken);
+    print(_itemId);
+  }
+
   void _onSuccessCallback(String publicToken, LinkSuccessMetadata metadata) {
+    //call cloud function to exchange public Token
+    _exchangePublicToken(publicToken);
     print("onSuccess: $publicToken, metadata: ${metadata.description()}");
   }
 
@@ -67,6 +93,23 @@ class _LinkAppMainState extends State<LinkAppMain> {
     }
   }
 
+  void _getTransactions(String _accessToken) async {
+    HttpsCallable callable =
+        FirebaseFunctions.instance.httpsCallable('getTransactions');
+    final message = await callable({'accessToken': _accessToken});
+    setState(() {
+      _transactionData = message.data;
+    });
+    /*
+    JsonEncoder encoder = new JsonEncoder.withIndent('  ');
+    for (var transaction in _transactionData) {
+      String prettyprint = encoder.convert(transaction);
+      print(prettyprint);
+    }
+    */
+    print('TOTAL NUMBER TRANSACTIONS: ' + _transactionData.length.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -76,12 +119,50 @@ class _LinkAppMainState extends State<LinkAppMain> {
           color: Colors.grey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            //mainAxisSize: MainAxisSize.min,
             children: <Widget>[
+              SizedBox(height: 15),
               Text('$_linkToken'),
               SizedBox(height: 15),
               ElevatedButton(
                 onPressed: _linkReady ? () => _plaidLinkToken.open() : null,
-                child: Text("Open Plaid Link (Link Token)"),
+                child: Text("Authenticate"),
+              ),
+              Text('$_accessToken'),
+              Text('$_itemId'),
+              SizedBox(height: 15),
+              ElevatedButton(
+                onPressed: _auth ? () => _getTransactions(_accessToken) : null,
+                child: Text("get Transactions"),
+              ),
+              SizedBox(height: 15),
+              Flexible(
+                flex: 1,
+                child: SingleChildScrollView(
+                  child: DataTable(
+                    columns: const <DataColumn>[
+                      DataColumn(label: Text('Date')),
+                      DataColumn(label: Text('Merchant Name')),
+                      DataColumn(label: Text('Amount')),
+                    ],
+                    rows: List<DataRow>.generate(
+                      _transactionData.length,
+                      (int index) => DataRow(
+                        cells: <DataCell>[
+                          DataCell(
+                            Text('${_transactionData[index]["date"]}'),
+                          ),
+                          DataCell(
+                            Text('${_transactionData[index]["merchant_name"]}'),
+                          ),
+                          DataCell(
+                            Text('${_transactionData[index]["amount"]}'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
