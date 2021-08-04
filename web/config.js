b779bad5e474aff042210c874ba6680d45d3d3fb
MMattLeeE <mmattleee@gmail.com>
2021-08-04 10:40:48 -0400

    update config for index

---
+++ web/config.js
@@ -0,0 +1,27 @@
+  // Your web app's Firebase configuration
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+  var firebaseConfig = {
+
+    apiKey: "AIzaSyBTZRmJJR0aJD7Sec0_xjcV46KAZ1ial38",
+
+    authDomain: "ctrlhub-5fd2a.firebaseapp.com",
+
+    projectId: "ctrlhub-5fd2a",
+
+    storageBucket: "ctrlhub-5fd2a.appspot.com",
+
+    messagingSenderId: "1025700724602",
+
+    appId: "1:1025700724602:web:491020263f36c025e0e0c3",
+
+    measurementId: "G-W7354QXHXS"
+
+  };
+
+  // Initialize Firebase
+
+  firebase.initializeApp(firebaseConfig);
+
+  firebase.analytics();
