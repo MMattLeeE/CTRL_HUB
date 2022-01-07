ad5c98f31be420ac19c2d68d1cfc19e755dbcaf4
MMattLeeE <mmattleee@gmail.com>
2022-01-07 18:57:05 -0500

    refactor and split into two widgets; page and box

---
+++ lib/login_page/login_box.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class LoginBox extends StatelessWidget {
+  const LoginBox({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 400,
+      width: 500,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey.withOpacity(0.0),
+          border: Border.all(color: Colors.white54),
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+        ),
+        child: Align(
+          alignment: Alignment(0, -.8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.fromLTRB(52, 12, 20, 24),
+                child: Text(
+                  'CTRL_hub',
+                  style: Theme.of(context).textTheme.headline2?.merge(
+                        TextStyle(
+                          color: Colors.white70,
+                        ),
+                      ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(52, 12, 30, 24),
+                child: Text(
+                  'Central front end for projects and utilites',
+                  style: Theme.of(context).textTheme.headline4?.merge(
+                        TextStyle(
+                          color: Colors.white60,
+                        ),
+                      ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(52, 12, 20, 24),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Sign in with Google'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/login_page/login_page.dart
+++ lib/login_page/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ctrl/login_page/login_box.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,6 +7,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      //the background image and the filter
       decoration: BoxDecoration(
         image: DecorationImage(
           image: AssetImage('assets/skyscraper.jpg'),
@@ -18,63 +20,9 @@ class LoginPage extends StatelessWidget {
           ),
         ),
       ),
+      //the center CTRL_hub UI element
       child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 400,
-              width: 500,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.0),
-                  border: Border.all(color: Colors.white54),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(20),
-                  ),
-                ),
-                child: Align(
-                  alignment: Alignment(0, -.8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(52, 12, 20, 24),
-                        child: Text(
-                          'CTRL_hub',
-                          style: Theme.of(context).textTheme.headline2?.merge(
-                                TextStyle(
-                                  color: Colors.white70,
-                                ),
-                              ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(52, 12, 30, 24),
-                        child: Text(
-                          'Central front end for projects and utilites',
-                          style: Theme.of(context).textTheme.headline4?.merge(
-                                TextStyle(
-                                  color: Colors.white60,
-                                ),
-                              ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(52, 12, 20, 24),
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: Text('Sign in with Google'),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+        child: LoginBox(),
       ),
     );
   }
