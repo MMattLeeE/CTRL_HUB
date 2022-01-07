3027f4c33bb888d9d04af1c973dd7c0bf4e38bea
MMattLeeE <mmattleee@gmail.com>
2022-01-07 18:44:13 -0500

    redesign center box

--- lib/login_page/login_page.dart
+++ lib/login_page/login_page.dart
@@ -23,11 +23,12 @@ class LoginPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
-              height: 500,
+              height: 400,
               width: 500,
               child: Container(
                 decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.85),
+                  color: Colors.grey.withOpacity(0.0),
+                  border: Border.all(color: Colors.white54),
                   borderRadius: BorderRadius.all(
                     Radius.circular(20),
                   ),
@@ -40,14 +41,25 @@ class LoginPage extends StatelessWidget {
                     children: [
                       Padding(
                         padding: EdgeInsets.fromLTRB(52, 12, 20, 24),
-                        child: Text('CTRL_hub',
-                            style: Theme.of(context).textTheme.headline2),
+                        child: Text(
+                          'CTRL_hub',
+                          style: Theme.of(context).textTheme.headline2?.merge(
+                                TextStyle(
+                                  color: Colors.white70,
+                                ),
+                              ),
+                        ),
                       ),
                       Padding(
                         padding: EdgeInsets.fromLTRB(52, 12, 30, 24),
                         child: Text(
-                            'Central front end for projects and utilites',
-                            style: Theme.of(context).textTheme.headline4),
+                          'Central front end for projects and utilites',
+                          style: Theme.of(context).textTheme.headline4?.merge(
+                                TextStyle(
+                                  color: Colors.white60,
+                                ),
+                              ),
+                        ),
                       ),
                       Padding(
                         padding: EdgeInsets.fromLTRB(52, 12, 20, 24),
