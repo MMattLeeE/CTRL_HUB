ea5b00bdf13ffe43dacb858552729016bba03951
MMattLeeE <mmattleee@gmail.com>
2022-01-07 04:01:12 -0500

    update login background and image

--- lib/login_page/login_page.dart
+++ lib/login_page/login_page.dart
@@ -6,46 +6,67 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.grey[700],
+      constraints: BoxConstraints.expand(),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/city.jpg'),
+          fit: BoxFit.none,
+          alignment: Alignment(-.5, .3),
+          scale: 1.2,
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(.75),
+            BlendMode.darken,
+          ),
+        ),
+      ),
       child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 500,
-              width: 500,
-              child: Container(
-                color: Colors.grey.withOpacity(0.25),
-                child: Align(
-                  alignment: Alignment(0, -.8),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(12, 12, 20, 24),
-                        child: Text('CTRL_hub',
-                            style: Theme.of(context).textTheme.headline2),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(12, 12, 30, 24),
-                        child: Text(
-                            'Central front end for projects and utilites',
-                            style: Theme.of(context).textTheme.headline4),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(12, 12, 20, 24),
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: Text('Sign in with Google'),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+        child: SizedBox(
+          height: 500,
+          width: 500,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.95),
+              borderRadius: BorderRadius.all(
+                Radius.circular(20),
               ),
             ),
-          ],
+            child: Align(
+              alignment: Alignment(0, -.8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(52, 12, 20, 24),
+                    child: Text(
+                      'CTRL_hub',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline2
+                          ?.copyWith(color: Colors.white70),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(52, 12, 30, 24),
+                    child: Text(
+                      'Central front end for projects and utilites',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4
+                          ?.copyWith(color: Colors.white70),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(52, 12, 20, 24),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Sign in with Google'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
